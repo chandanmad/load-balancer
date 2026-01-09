15b16fad7a47297affc5d0f7e84fd4be4f8cd463
Chandan Madhesia <chandanmadhesia@gmail.com>
2026-01-09 22:58:38 +0100

    Commiting local changes

--- crates/load-balancer/examples/upstream.rs
+++ crates/load-balancer/examples/upstream.rs
@@ -1,4 +1,14 @@
+use std::time::Duration;
+
+use axum::{
+    Json, Router,
+    extract::{Path, Query},
+    routing::{self},
+};
 use clap::Parser;
+use reqwest::StatusCode;
+use serde::{Deserialize, Serialize};
+use tokio::net::TcpListener;
 
 #[derive(Parser, Debug)]
 #[command(name = "upstream", long_about = None)]
@@ -10,7 +20,49 @@ struct Args {
     ip: String,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
     println!("{:?}", args);
+
+    let listener = TcpListener::bind(format!("{}:{}", args.ip, args.port))
+        .await
+        .unwrap();
+    let app = Router::new().route("/{*key}", routing::get(handler));
+    let _ = axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Deserialize, Serialize)]
+struct Params {
+    status: Option<u16>,
+    latency_ms: Option<u64>,
+}
+
+#[derive(Serialize)]
+struct Response {
+    path: String,
+    status: u16,
+    lateny_ms: Option<u64>,
+}
+
+async fn handler(
+    Path(path): Path<String>,
+    Query(params): Query<Params>,
+) -> (StatusCode, Json<Response>) {
+    let status = params
+        .status
+        .and_then(|s| StatusCode::from_u16(s).ok())
+        .unwrap_or(StatusCode::OK);
+
+    if let Some(delay) = params.latency_ms {
+        tokio::time::sleep(Duration::from_millis(delay)).await;
+    }
+    (
+        status,
+        Json(Response {
+            path,
+            status: status.as_u16(),
+            lateny_ms: params.latency_ms,
+        }),
+    )
 }
