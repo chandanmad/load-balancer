a32509733b6b6999651076f5b510135d9321f16d
Chandan Madhesia <chandanmadhesia@gmail.com>
2026-01-18 19:57:06 +0100

    Commit local changes

--- crates/load-balancer/sql/usage.sql
+++ crates/load-balancer/sql/usage.sql
@@ -3,7 +3,7 @@ CREATE TABLE Usage (
     api_key CHAR(36) NOT NULL,
     plan_id INTEGER NOT NULL,
     date_time DATETIME NOT NULL,
-    total_requests INTEGER,
-    total_data_mb REAL,
+    total_requests INTEGER NOT NULL DEFAULT 0,
+    total_data_mb REAL NOT NULL DEFAULT 0,
     PRIMARY KEY (account_id, api_key, plan_id, date_time)
 );
